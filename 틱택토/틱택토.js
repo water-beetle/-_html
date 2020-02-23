d099216a1d6fde1c2f897c8f60b96d44b4c378a5
water-beetle <60806919+water-beetle@users.noreply.github.com>
2020-02-23 23:32:29 +0900

    Add files via upload

--- 틱택토/틱택토.js
+++ 틱택토/틱택토.js
@@ -2,24 +2,64 @@ var body = document.body;
 var table = document.createElement('table');
 var columns = [];
 var rows = [];
+var turn = 'X'
 
 var 비동기콜백 = function(e) {
-    console.log(e.target);
+    console.log(e.target); //row
+	console.log(e.target.parentNode); //columns
+	console.log(e.target.parentNode.parentNode); //table
+	
+	var columns_num = columns.indexOf(e.target.parentNode);
+	var rows_num = rows[columns_num].indexOf(e.target);
+	console.log(columns_num)
+	console.log(rows_num)
+	
+	if(rows[columns_num][rows_num].textContent != '') { //칸이 이미 채워져 있는가
+		console.log('빈칸아닙니다');  
+	} else {
+		console.log('빈칸입니다');
+		rows[columns_num][rows_num].textContent = turn;
+		if(turn == 'O')
+			turn = 'X';
+		else
+			turn = 'O';
+		
+	}
+	
+// 세칸이 다 채워졌는가?
+	var isWin = false;
+	//가로줄 검사
+	if(rows[columns_num][0] == turn && rows[columns_num][1] == turn && rows[columns_num][0] == turn) {
+		isWin = true;	
+	}
+	//세로줄 검사
+	if(rows[0][rows_num] == turn && rows[1][rows_num] == turn && rows[2][rows_num] == turnn) {
+		isWin = true;	
+	}
+	//대각선 검사
+	if(columns_num == rows_num) {
+		if(rows[0][0] == turn && rows[1][1] == turn && rows[2][2] == turn) {
+			isWin = true;
+		}
+	}
+	if(isWin)
+		console.log(turn + '의 승리')
 }
 
 
 for(i=1; i<=3; i+=1) {
     var column = document.createElement('tr');
-    columns.push([]);
+    columns.push(column);
     rows.push([]);
     for(j=1; j<=3 ;j+=1) {
         var row = document.createElement('td');
         row.addEventListener('click',비동기콜백);
-        columns[i-1].push(row);
+        rows[i-1].push(row);
         column.appendChild(row);
     }
      table.appendChild(column);
 }
 body.appendChild(table)
-console.log(columns);
+
+
 
